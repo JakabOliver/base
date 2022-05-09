f82eea0da1da0a7d4ddde2ecd42780a58b5d5bb5
fruzsi0926 <fruzska.karajz@gmail.com>
2022-05-09 17:22:11 +0200

    overrideSpeedLimit function change

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -25,11 +25,13 @@ public class TrainSensorImpl implements TrainSensor {
 		this.speedLimit = speedLimit;
 		controller.setSpeedLimit(speedLimit);
 		
-        if (controller.getReferenceSpeed() / 2 > speedLimit) {
-            user.setAlarmState(true);
-        }
 		if(speedLimit < 0 || speedLimit > 500)
 			user.setAlarmState(true);
+
+        else if (controller.getReferenceSpeed() / 2 > speedLimit) {
+            user.setAlarmState(true);
+        }
+		
 	}
 
 }
