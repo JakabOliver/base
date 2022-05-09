ea6a007e6a5de57b173f84a125c675e3dc565846
Oliver Jakab <oliver.jakab.r@gmail.com>
2022-05-09 17:39:35 +0200

    TrainSensorTest four test cases added

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -6,32 +6,32 @@ import hu.bme.mit.train.interfaces.TrainUser;
 
 public class TrainSensorImpl implements TrainSensor {
 
-	private TrainController controller;
-	private TrainUser user;
-	private int speedLimit = 5;
+    private TrainController controller;
+    private TrainUser user;
+    private int speedLimit = 5;
 
-	public TrainSensorImpl(TrainController controller, TrainUser user) {
-		this.controller = controller;
-		this.user = user;
-	}
+    public TrainSensorImpl(TrainController controller, TrainUser user) {
+        this.controller = controller;
+        this.user = user;
+    }
 
-	@Override
-	public int getSpeedLimit() {
-		return speedLimit;
-	}
+    @Override
+    public int getSpeedLimit() {
+        return speedLimit;
+    }
 
-	@Override
-	public void overrideSpeedLimit(int speedLimit) {
-		this.speedLimit = speedLimit;
-		controller.setSpeedLimit(speedLimit);
-		
-		if(speedLimit < 0 || speedLimit > 500)
-			user.setAlarmState(true);
+    @Override
+    public void overrideSpeedLimit(int speedLimit) {
+        this.speedLimit = speedLimit;
+        controller.setSpeedLimit(speedLimit);
 
-        else if (controller.getReferenceSpeed() / 2 > speedLimit) {
+        if (speedLimit < 0 || speedLimit > 500)
+            user.setAlarmState(true);
+
+        else if ((controller.getReferenceSpeed() / 2.00) > speedLimit) {
             user.setAlarmState(true);
         }
-		
-	}
+
+    }
 
 }
--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,48 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
+    TrainController mockTc;
+    TrainUser mockTu;
+    TrainSensorImpl trainSensor;
 
     @Before
     public void before() {
-        // TODO Add initializations
+        mockTc = mock(TrainController.class);
+        mockTu = mock(TrainUser.class);
+        trainSensor = new TrainSensorImpl(mockTc, mockTu);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void absolutMarginDownTest() {
+        trainSensor.overrideSpeedLimit(-10);
+        verify(mockTu, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void absolutMarginUpTest() {
+        trainSensor.overrideSpeedLimit(510);
+        verify(mockTu, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void relativMarginDownTest() {
+        when(mockTc.getReferenceSpeed()).thenReturn(5);
+        trainSensor.overrideSpeedLimit(2);
+        verify(mockTu, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void higherLimitTest() {
+        when(mockTc.getReferenceSpeed()).thenReturn(5);
+        trainSensor.overrideSpeedLimit(7);
+        verify(mockTu, times(0)).setAlarmState(false);
     }
 }
